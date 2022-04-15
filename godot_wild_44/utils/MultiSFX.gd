0f125b2c8805326ec7424f2034ac50aa8730ec78
SuddenlyNothing <72772195+SuddenlyNothing@users.noreply.github.com>
2022-04-15 00:03:28 -0700

    Remove play on enter

--- godot_wild_44/utils/MultiSFX.gd
+++ godot_wild_44/utils/MultiSFX.gd
@@ -5,12 +5,7 @@ export(Array, AudioStream) var audio
 var rng := RandomNumberGenerator.new()
 
 
-func _ready() -> void:
-	play()
-
-
 func play(from_position: float = 0.0) -> void:
-	rng.randomize()
 	if stream is AudioStreamRandomPitch:
 		stream.audio_stream = audio[rng.randi_range(0, len(audio) - 1)]
 	else:
